0181bc735b8afeb74e32a0661d9a839cea12bee5
Marvball <anmeldungen@diehaselbachs.de>
2024-12-11 14:34:08 +0100

    Update Day_4_1.py

--- 2024/Day_4_1.py
+++ 2024/Day_4_1.py
@@ -1,4 +1,4 @@
-#DAY_4_1
+#DAY_4_1_wip
 
 import re
 
@@ -55,7 +55,7 @@ def vertical_top_bottom():
           if matrix[row+1][column] == "M":
             if matrix[row+2][column] == "A":
               if matrix[row+3][column] == "S":
-                #print(f"{row=}{column=}")
+                #print(f"{row=} {column=}")
                 answer += 1
         except:
           continue
@@ -69,20 +69,76 @@ def vertical_bottom_top():
           if matrix[row-1][column] == "M":
             if matrix[row-2][column] == "A":
               if matrix[row-3][column] == "S":
-                #print(f"{row=}{column=}")
+                #print(f"{row=} {column=}")
                 answer += 1
         except:
           continue
-        
-   
 
+def diagonal_tl_br():
+  global answer
+  for row in range(len(matrix)):
+    for column in range(len(matrix[row])):
+      if matrix[row][column] == "X":
+        try:
+          if matrix[row+1][column+1] == "M":
+            if matrix[row+2][column+2] == "A":
+              if matrix[row+3][column+3] == "S":
+                #print(f"{row=} {column=}")
+                answer += 1
+        except:
+          continue 
 
+def diagonal_bl_tr():
+  global answer
+  for row in range(len(matrix)):
+    for column in range(len(matrix[row])):
+      if matrix[row][column] == "X":
+        try:
+          if matrix[row-1][column+1] == "M":
+            if matrix[row-2][column+2] == "A":
+              if matrix[row-3][column+3] == "S":
+                #print(f"{row=} {column=}")
+                answer += 1
+        except:
+          continue
+
+def diagonal_br_tl():
+  global answer
+  for row in range(len(matrix)):
+    for column in range(len(matrix[row])):
+      if matrix[row][column] == "X":
+        try:
+          if matrix[row-1][column-1] == "M" and column-1 >= 0:
+            if matrix[row-2][column-2] == "A" and column-2 >= 0:
+              if matrix[row-3][column-3] == "S" and column-2 >= 0:
+                print(f"{row=} {column=}")
+                answer += 1
+        except:
+          continue
+
+def diagonal_tr_bl():
+  global answer
+  for row in range(len(matrix)):
+    for column in range(len(matrix[row])):
+      if matrix[row][column] == "X":
+        try:
+          if matrix[row+1][column-1] == "M" and column-1 >= 0:
+            if matrix[row+2][column-2] == "A" and column-2 >= 0:
+              if matrix[row+3][column-3] == "S" and column-2 >= 0:
+                print(f"{row=} {column=}")
+                answer += 1
+        except:
+          continue
 
 
 horizontal_left_right()
 horizontal_right_left()
 vertical_top_bottom()
 vertical_bottom_top()
+diagonal_tl_br()
+diagonal_bl_tr()
+diagonal_br_tl()
+diagonal_tr_bl()
 print(f"Final answer: {answer}")
 
 
